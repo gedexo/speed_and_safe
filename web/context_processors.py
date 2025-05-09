f115fd492cbc9f64982572d938f0e4cff1c88479
Midhlajrahman <mIdhlajrahman26@gmail.com>
2025-05-09 17:55:28 +0530

    up

--- web/context_processors.py
+++ web/context_processors.py
@@ -1,7 +1,7 @@
 from .models import Product
 
 def main_context(request):
-    footer_products = Product.objects.all()
+    footer_products = Product.objects.all()[:8]
     return {
         "footer_products": footer_products
     }
